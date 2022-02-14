adc9d86b426765967f205b78cfd761f202d2ac46
Jeremy <1021082837@qq.com>
2022-02-14 21:53:00 +0800

    The script delete all .pth files in original results

--- simplify.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for root, dirs, files in os.walk(r"/Users/jeremy/Desktop/target clean-label attacks/results"):
-    for file in files:
-        pos = file.find('.')
-        if file[pos:] == ".pth":
-            os.remove(os.path.join(root, file))
-            # print(os.path.join(root, file))
